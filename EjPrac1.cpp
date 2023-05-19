af8156377eb1ff4b6e67153b899ad7de4251357d
Hongni3 <lorena.flores16151@est.uca.edu.ni>
2023-05-19 10:31:09 -0600

    Primer ejercicio

---
+++ EjPrac1.cpp
@@ -0,0 +1,9 @@
+/*Escribir un programa que muestre los números del 1 al 10 en orden ascendente.*/
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
